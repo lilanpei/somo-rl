6ccbe1f103c50dd27e06ca9c7f3a215b227e5549
lilanpei <lilanpei@gmail.com>
2022-11-25 12:18:43 +0100

    Add SaveOnBestTrainingRewardCallback for multi-objects manipulation

--- somo_rl/train_policy.py
+++ somo_rl/train_policy.py
@@ -13,6 +13,7 @@ from pathlib import Path
 
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.results_plotter import load_results, ts2xy
 from stable_baselines3.common.env_util import Monitor
 from stable_baselines3.common.callbacks import (
     BaseCallback,
@@ -29,6 +30,51 @@ from somo_rl.utils import parse_config, construct_policy_model
 
 from user_settings import EXPERIMENT_ABS_PATH
 import gym
+
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq:
+    :param log_dir: Path to the folder where the model will be saved.
+      It must contain the file created by the ``Monitor`` wrapper.
+    :param verbose: Verbosity level: 0 for no output, 1 for info messages, 2 for debug messages
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, "best_model")
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), "timesteps")
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose >= 1:
+                print(f"Num timesteps: {self.num_timesteps}")
+                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose >= 1:
+                    print(f"Saving new best model to {self.save_path}")
+                  self.model.save(self.save_path)
+
+        return True
 
 
 class TensorboardCallback(BaseCallback):
@@ -301,7 +347,18 @@ def run(
         save_freq=run_config["checkpoint_cb"]["save_freq"],
         save_path=checkpoints_dir,
     )
-    callback = CallbackList([eval_callback, checkpoint_callback, TensorboardCallback()])
+
+    savebest_callback = SaveOnBestTrainingRewardCallback(
+        check_freq=run_config["eval_cb"]["eval_freq"],
+        log_dir=models_dir
+    )
+
+    tensorboardCallback = TensorboardCallback()
+
+    if num_threads > 1:
+        callback = CallbackList([savebest_callback, checkpoint_callback, tensorboardCallback])
+    else:
+        callback = CallbackList([eval_callback, checkpoint_callback, tensorboardCallback])
 
     policy_kwargs = {}
     if "policy_kwargs" in run_config:
