1ad6105fb4b562708b84ce46eda547295a495c26
lilanpei <lilanpei@gmail.com>
2022-12-03 14:08:37 +0100

    Reduce memory using in behavior cloning

--- somo_rl/post_processing/behavior_cloning.py
+++ somo_rl/post_processing/behavior_cloning.py
@@ -4,27 +4,28 @@ import argparse
 import numpy as np
 import torch as th
 import pandas as pd
-import torch.nn as nn
-import torch.optim as optim
-from stable_baselines3 import PPO, A2C
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data.dataset import Dataset, random_split
+from torch import nn
+from torch import optim
+from stable_baselines3 import PPO, A2C
 from stable_baselines3.common.utils import set_random_seed
 
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 sys.path.insert(0, path)
 
-from user_settings import EXPERIMENT_ABS_PATH
+from user_settings import EXPERIMENT_ABS_PATH, EXPERT_ABS_PATH
 from somo_rl.utils import construct_policy_model
 from somo_rl.utils.load_env import load_env
 from somo_rl.utils.evaluate_policy import evaluate_policy
 from somo_rl.utils.load_run_config_file import load_run_config_file
 
 
-def unison_shuffled_copies(X, y):
-    assert len(X) == len(y)
-    p = np.random.permutation(len(X))
-    return X[p], y[p]
+def unison_shuffled_copies(expert_dataset):
+    assert len(expert_dataset.observations) == len(expert_dataset.actions)
+    p = np.random.permutation(len(expert_dataset.observations))
+
+    return ExpertDataSet(expert_dataset.observations[p], expert_dataset.actions[p])
 
 class ExpertDataSet(Dataset):
     def __init__(self, expert_observations, expert_actions):
@@ -47,7 +48,6 @@ class Policy_rollout:
         self.env = load_env(self.run_config, render=render, debug=debug)
         self.load_rollouts()
 
-
     def load_rollouts(self):
         saved_data_path = os.path.join(self.run_dir, f"expert_data_{self.run_config['object']}.npz")
         if os.path.isfile(saved_data_path):
@@ -65,14 +65,7 @@ class Policy_rollout:
             actions_df = pd.concat(actions_df_list)
             observations_df = pd.concat(observations_df_list)
 
-            expert_observations = np.empty((observations_df.shape[0],) + self.env.observation_space.shape)
-            expert_actions = np.empty((actions_df.shape[0],) + (self.env.action_space.shape[0],))
-
-            for (idxRow, obs), (_, act) in zip(observations_df.iterrows(), actions_df.iterrows()):
-                expert_observations[idxRow] = obs.to_numpy()
-                expert_actions[idxRow] = act.to_numpy()
-
-            expert_observations, expert_actions = unison_shuffled_copies(expert_observations, expert_actions)
+            expert_observations, expert_actions = observations_df.to_numpy(), actions_df.to_numpy()
 
             np.savez_compressed(
                 os.path.join(self.run_dir, f"expert_data_{self.run_config['object']}"),
@@ -81,17 +74,7 @@ class Policy_rollout:
             )
 
         self.expert_dataset = ExpertDataSet(expert_observations, expert_actions)
-
-        train_size = int(0.8 * len(self.expert_dataset))
-        test_size = len(self.expert_dataset) - train_size
-
-        self.train_expert_dataset, self.test_expert_dataset = random_split(
-            self.expert_dataset, [train_size, test_size]
-        )
-        # self.train_expert_dataset = self.test_expert_dataset = self.expert_dataset
-
-        print(f"train_expert_dataset_{self.run_config['object']}: {len(self.train_expert_dataset)}")
-        print(f"test_expert_dataset_{self.run_config['object']}: {len(self.test_expert_dataset)}")
+        print(f"size of expert_dataset_{self.run_config['object']}: {len(self.expert_dataset)}")
 
 
 class Pretrain_agent:
@@ -178,7 +161,7 @@ class Pretrain_agent:
 
                 test_loss = self.criterion(action_prediction, target)
         test_loss /= len(test_loader.dataset)
-        print(f"Test set: Average loss: {test_loss:.4f}")
+        print(f"Test set: Average loss: {test_loss:.6f}")
 
     def behavior_cloning(self):
         # Define an Optimizer and a learning rate schedule.
@@ -187,10 +170,10 @@ class Pretrain_agent:
 
         # use PyTorch `DataLoader` to load previously created `ExpertDataset` for training and testing
         train_loader = th.utils.data.DataLoader(
-            dataset=self.train_expert_dataset, batch_size=self.batch_size, shuffle=True, **self.kwargs
+            dataset=self.train_expert_dataset, batch_size=self.batch_size, shuffle=False, **self.kwargs
         )
         test_loader = th.utils.data.DataLoader(
-            dataset=self.test_expert_dataset, batch_size=self.test_batch_size, shuffle=True, **self.kwargs,
+            dataset=self.test_expert_dataset, batch_size=self.test_batch_size, shuffle=False, **self.kwargs,
         )
 
         # Now we are finally ready to train the policy model.
@@ -203,7 +186,7 @@ class Pretrain_agent:
         self.student.policy = self.model
 
 
-def run(run_IDs, exp_abs_path=EXPERIMENT_ABS_PATH, seed=100, render=False, debug=False, n_eval_episodes=10):
+def run(run_IDs, exp_abs_path=EXPERIMENT_ABS_PATH, seed=100, render=False, debug=False, n_eval_episodes=10, shuffle=True):
     print(f"seed = {seed}, n_eval_episodes = {n_eval_episodes}")
     set_random_seed(seed)
     num_experts = len(run_IDs)
@@ -214,25 +197,34 @@ def run(run_IDs, exp_abs_path=EXPERIMENT_ABS_PATH, seed=100, render=False, debug
         expert_policy.append(Policy_rollout(exp_abs_path=exp_abs_path, run_ID=run_IDs[idx], render=render, debug=debug))
 
     # Evaluate the expert trained for multi-objects manipulation
-    baseline_runID = [run_IDs[0][0], "PPO_multi_objects", run_IDs[0][-1]]
-    baseline_run_dir, baseline_run_config = load_run_config_file(baseline_runID)
-    for i in range(num_experts):
-        alg = construct_policy_model.ALGS[expert_policy[i].run_config["alg"]]
-        mean_reward_expert, std_reward_expert, mean_z_rotation_expert, std_z_rotation_expert = evaluate_policy(
-            model=alg.load(os.path.join(baseline_run_dir, "models/best_model")), run_ID=run_IDs[i], n_eval_episodes=n_eval_episodes, deterministic=False, render=render)
-        print(f"Mean reward {baseline_run_config['object']} expert on {expert_policy[i].run_config['object']} env = {mean_reward_expert} +/- {std_reward_expert}")
-        print(f"z rotation {baseline_run_config['object']} expert on {expert_policy[i].run_config['object']} env = {mean_z_rotation_expert} +/- {std_z_rotation_expert}")
+    # baseline_runID = [run_IDs[0][0], "PPO_multi_objects", run_IDs[0][-1]]
+    baseline_run_group_name = (list(run_IDs[0][1].split("_")))[0] + "_" + "_".join([(list(run_IDs[i][1].split("_")))[-1] for i in range(num_experts)])
+    baseline_runID = [run_IDs[0][0], baseline_run_group_name, run_IDs[0][-1]]
+    try:
+        baseline_run_dir, baseline_run_config = load_run_config_file(baseline_runID)
+        for i in range(num_experts):
+            alg = construct_policy_model.ALGS[expert_policy[i].run_config["alg"]]
+            mean_reward_expert, std_reward_expert, mean_z_rotation_expert, std_z_rotation_expert = evaluate_policy(
+                model=alg.load(os.path.join(baseline_run_dir, "models/best_model")), run_ID=run_IDs[i],
+                n_eval_episodes=n_eval_episodes, deterministic=False, render=render)
+            print(
+                f"Mean reward {baseline_run_config['object']} expert on {expert_policy[i].run_config['object']} env = {mean_reward_expert:.2f} +/- {std_reward_expert:.2f}")
+            print(
+                f"z rotation {baseline_run_config['object']} expert on {expert_policy[i].run_config['object']} env = {mean_z_rotation_expert:.2f} +/- {std_z_rotation_expert:.2f}")
+    except:
+        print(f"Exception : The expert trained for multi-objects manipulation does not exist : {baseline_runID}")
 
     # Evaluate the expert trained for individual-object manipulation
     for idx in range(num_experts):
         for j in range(num_experts):
             alg = construct_policy_model.ALGS[expert_policy[idx].run_config["alg"]]
             mean_reward_expert, std_reward_expert, mean_z_rotation_expert, std_z_rotation_expert = evaluate_policy(
-                model=alg.load(os.path.join(expert_policy[idx].run_dir, "models/best_model")), run_ID=run_IDs[j], n_eval_episodes=n_eval_episodes, deterministic=False, render=render)
+                model=alg.load(os.path.join(expert_policy[idx].run_dir, "models/best_model")), run_ID=run_IDs[j],
+                n_eval_episodes=n_eval_episodes, deterministic=False, render=render)
             print(
-                f"Mean reward {expert_policy[idx].run_config['object']} expert on {expert_policy[j].run_config['object']} env = {mean_reward_expert} +/- {std_reward_expert}")
+                f"Mean reward {expert_policy[idx].run_config['object']} expert on {expert_policy[j].run_config['object']} env = {mean_reward_expert:.2f} +/- {std_reward_expert:.2f}")
             print(
-                f"z rotation {expert_policy[idx].run_config['object']} expert on {expert_policy[j].run_config['object']} env = {mean_z_rotation_expert} +/- {std_z_rotation_expert}")
+                f"z rotation {expert_policy[idx].run_config['object']} expert on {expert_policy[j].run_config['object']} env = {mean_z_rotation_expert:.2f} +/- {std_z_rotation_expert:.2f}")
 
     # Construct a student agent for behavior cloning
     student = construct_policy_model.construct_policy_model(expert_policy[0].run_config["alg"],
@@ -241,54 +233,73 @@ def run(run_IDs, exp_abs_path=EXPERIMENT_ABS_PATH, seed=100, render=False, debug
 
     # Evaluate the initial student policy
     for idx in range(num_experts):
-        mean_reward_student, std_reward_student, mean_z_rotation_student, std_z_rotation_student = evaluate_policy(model=student, run_ID=run_IDs[idx], n_eval_episodes=n_eval_episodes, deterministic=False, render=render)
-        print(f"Mean reward student on {expert_policy[idx].run_config['object']} env = {mean_reward_student} +/- {std_reward_student}")
-        print(f"z rotation student on {expert_policy[idx].run_config['object']} env = {mean_z_rotation_student} +/- {std_z_rotation_student}")
+        mean_reward_student, std_reward_student, mean_z_rotation_student, std_z_rotation_student = evaluate_policy(
+            model=student, run_ID=run_IDs[idx], n_eval_episodes=n_eval_episodes, deterministic=False, render=render)
+        print(
+            f"Mean reward student on {expert_policy[idx].run_config['object']} env = {mean_reward_student:.2f} +/- {std_reward_student:.2f}")
+        print(
+            f"z rotation student on {expert_policy[idx].run_config['object']} env = {mean_z_rotation_student:.2f} +/- {std_z_rotation_student:.2f}")
 
     # Prepare the dataset for pretraining
+    print(f"num_experts = {num_experts}")
     if num_experts > 1:
-        saved_pretraining_data_path = f"pretraining_data_{'_'.join([expert_policy[idx].run_config['object'] for idx in range(num_experts)])}.npz"
+        os.makedirs(EXPERT_ABS_PATH, exist_ok=True)
+        saved_pretraining_data_path = os.path.join(EXPERT_ABS_PATH,
+                                                   f"pretraining_data_{'_'.join([expert_policy[idx].run_config['object'] for idx in range(num_experts)])}.npz")
         if os.path.isfile(saved_pretraining_data_path):
-            expert_observations_dataset, expert_actions_dataset = np.load(saved_pretraining_data_path)["expert_observations"], np.load(saved_pretraining_data_path)["expert_actions"]
+            expert_observations_dataset, expert_actions_dataset = np.load(saved_pretraining_data_path)[
+                                                                      "expert_observations"], \
+                                                                  np.load(saved_pretraining_data_path)["expert_actions"]
             print(f"Using saved data from : {saved_pretraining_data_path}")
         else:
             print(f"Preparing dataset for pretraining from each expert dataset")
-            expert_actions_dataset = np.concatenate([expert_policy[i].expert_dataset.actions for i in range(num_experts)], axis=0)
-            expert_observations_dataset = np.concatenate([expert_policy[i].expert_dataset.observations for i in range(num_experts)], axis=0)
-        
-            # shuffle the pretraining dataset
-            expert_actions_dataset, expert_observations_dataset = unison_shuffled_copies(expert_actions_dataset, expert_observations_dataset)
-        
+            expert_actions_dataset = np.concatenate(
+                [expert_policy[i].expert_dataset.actions for i in range(num_experts)], axis=0, dtype=np.double)
+            expert_observations_dataset = np.concatenate(
+                [expert_policy[i].expert_dataset.observations for i in range(num_experts)], axis=0, dtype=np.double)
+
             np.savez_compressed(
-                f"pretraining_data_{'_'.join([expert_policy[idx].run_config['object'] for idx in range(num_experts)])}",
+                os.path.join(EXPERT_ABS_PATH,
+                             f"pretraining_data_{'_'.join([expert_policy[idx].run_config['object'] for idx in range(num_experts)])}"),
                 expert_observations=expert_observations_dataset,
                 expert_actions=expert_actions_dataset,
             )
-        
         expert_dataset = ExpertDataSet(expert_observations_dataset, expert_actions_dataset)
-        train_size = int(0.8 * len(expert_dataset))
-        test_size = len(expert_dataset) - train_size
-
-        train_expert_dataset, test_expert_dataset = random_split(
-            expert_dataset, [train_size, test_size]
-        )
-        # train_expert_dataset = test_expert_dataset = expert_dataset
     else:
-        train_expert_dataset, test_expert_dataset = expert_policy[0].train_expert_dataset, expert_policy[0].test_expert_dataset
+        expert_dataset = expert_policy[0].expert_dataset
+
+    # Shuffle the pretraining dataset
+    if shuffle:
+        print("Shuffling the pretraining dataset")
+        expert_dataset = unison_shuffled_copies(expert_dataset)
+
+    train_size = int(0.8 * len(expert_dataset))
+    test_size = len(expert_dataset) - train_size
+
+    train_expert_dataset, test_expert_dataset = random_split(
+        expert_dataset, [train_size, test_size]
+    )
+    # train_expert_dataset = test_expert_dataset = expert_dataset
 
     print(f"train dataset for pretraining: {len(train_expert_dataset)}")
     print(f"test dataset for pretraining: {len(test_expert_dataset)}")
 
     # Pretrain the student agent
     pretrain_agent = Pretrain_agent(student, train_expert_dataset, test_expert_dataset)
-    student.save(f"student_{'_'.join([expert_policy[idx].run_config['object'] for idx in range(num_experts)])}")
-    print(f"Saving agent : student_{'_'.join([expert_policy[idx].run_config['object'] for idx in range(num_experts)])}")
+    student_save_path = os.path.join(EXPERT_ABS_PATH,
+                                     f"student_{'_'.join([expert_policy[idx].run_config['object'] for idx in range(num_experts)])}")
+    student.save(student_save_path)
+    print(f"Saving agent at: {student_save_path}")
 
     # Evaluate the trained student policy
     for idx in range(num_experts):
-        mean_reward_student, std_reward_student, mean_z_rotation_student, std_z_rotation_student = evaluate_policy(model=pretrain_agent.student, run_ID=run_IDs[idx], n_eval_episodes=n_eval_episodes, deterministic=False, render=render)
-        print(f"Mean reward student on {expert_policy[idx].run_config['object']} env = {mean_reward_student} +/- {std_reward_student}")
-        print(f"z rotation student on {expert_policy[idx].run_config['object']} env = {mean_z_rotation_student} +/- {std_z_rotation_student}")
+        mean_reward_student, std_reward_student, mean_z_rotation_student, std_z_rotation_student = evaluate_policy(
+            model=pretrain_agent.student, run_ID=run_IDs[idx], n_eval_episodes=n_eval_episodes, deterministic=False,
+            render=render)
+        print(
+            f"Mean reward student on {expert_policy[idx].run_config['object']} env = {mean_reward_student:.2f} +/- {std_reward_student:.2f}")
+        print(
+            f"z rotation student on {expert_policy[idx].run_config['object']} env = {mean_z_rotation_student:.2f} +/- {std_z_rotation_student:.2f}")
 
 
 if __name__ == "__main__":
@@ -385,6 +396,13 @@ if __name__ == "__main__":
         default=10
     )
 
+    parser.add_argument(
+        "-s",
+        "--shuffle",
+        help="shuffle the expert dataset",
+        action="store_true",
+    )
+
     arg = parser.parse_args()
     seed = int(arg.seed) if arg.seed is not None else None
     n_eval_episodes = int(arg.n_eval_episodes) if arg.n_eval_episodes is not None else None
@@ -398,4 +416,4 @@ if __name__ == "__main__":
         run_ID_3 = [arg.exp_name_3, arg.run_group_name_3, arg.run_name_3]
         run_IDs.append(run_ID_3)
 
-    run(run_IDs=run_IDs, exp_abs_path=arg.exp_abs_path, render=arg.render, seed=seed, n_eval_episodes=n_eval_episodes)
+    run(run_IDs=run_IDs, exp_abs_path=arg.exp_abs_path, render=arg.render, seed=seed, n_eval_episodes=n_eval_episodes, shuffle=arg.shuffle)
